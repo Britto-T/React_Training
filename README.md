cb7596fc01ffea707e096ed02cd4e46d909db158
Britto-T <meetbrittohere@gmail.com>
2022-08-20 09:46:32 +0530

    Calling api using axios

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ React_Training in Javascript
 
 # Installation
 npx create-react-app react_training --template redux
+
+# USeful APIS
+1.https://datausa.io/api/data?drilldowns=Nation&measures=Population
+2.https://api.first.org/data/v1/countries
+3.https://countriesnow.space/api/v0.1/countries/capital
+4.https://restcountries.com/#api-endpoints-v3-capital-city
+5.https://restcountries.com/v3.1/all
+6.https://restcountries.com/v2/currency/pen
+7.https://restcountries.com/v2/lang/es
+8.https://countriesnow.space/api/v0.1/countries/population/cities
+API Websites
+1.https://restcountries.com/#api-endpoints-v3-list-of-codes
+2.https://documenter.getpostman.com/view/1134062/T1LJjU52#1796dc54-b450-4774-8901-268e5d92e535
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
-const Welcome=()=>{
-    return(
-        <>
-            <h1>Welcome</h1>
-            <Button variant="primary">Primary</Button>{' '}
-            <Button variant="secondary">Secondary</Button>{' '}
-            <Button variant="success">Success</Button>{' '}
-            <Button variant="warning">Warning</Button>{' '}
-            <Button variant="danger">Danger</Button>{' '}
-            <Button variant="info">Info</Button>{' '}
-            <Button variant="light">Light</Button>{' '}
-            <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
-        </>
-    )
-}
+const Welcome = () => {
+  const [population, setPopulation] = useState([]);
 
-export default Welcome
+  useEffect(() => {
+    axios
+      .get("https://countriesnow.space/api/v0.1/countries/capital")
+      .then((res) => {
+        if (res !== "" || res !== null) {
+          console.log(res.data.data);
+          setPopulation(res.data.data);
+        }
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome</h1>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button variant="warning">Warning</Button>{" "}
+      <Button variant="danger">Danger</Button>{" "}
+      <Button variant="info">Info</Button>{" "}
+      <Button variant="light">Light</Button>{" "}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      {population.map((item, index) => {
+        return (
+          <div key={index}>
+            <h4>{item.name}</h4>
+            <h4>{item.capital}</h4>
+            <br />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Welcome;
